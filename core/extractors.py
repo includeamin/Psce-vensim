fdc23e2eedb520744b377dd74ecf9e8911d7ffbd
includeamin <aminjamal10@gmail.com>
2019-11-13 01:05:28 +0330

    extractor

--- core/extractors.py
+++ core/extractors.py
@@ -8,6 +8,12 @@ class MeteoExtractor:
         self.FName: str = f_name
         self.Coefficients: dict = coefficients
 
+    def get_weather_et0(self):
+        weather_data = CABOWeatherDataProvider(self.FName, fpath=self.Path)
+        data = weather_data.export()
+        et0s = [item["ET0"] for item in data]
+        return et0s
+
     def writer(self):
         weather_data = CABOWeatherDataProvider(self.FName, fpath=self.Path)
         keys = weather_data.export()[0].keys()
@@ -19,23 +25,38 @@ class MeteoExtractor:
     def loader(self):
         with open(f'{self.FName}.csv', 'w') as f:
             file = csv.reader(f)
+            print(file)
             return file
 
     def load_general_lookup(self):
-        csv_weather_file = self.loader()
-        et0s = [item["ET0"] for item in csv_weather_file]
+        et0s = self.get_weather_et0()
         final_result = {}
-
-        for i in range(len(self.Coefficients.keys())):
-            temp = [item * self.Coefficients[i] for item in et0s]
-            final_result[i] = temp
+        keys = self.Coefficients.keys()
+        for item in keys:
+            temp = [float(self.Coefficients[item]) * float(i) for i in et0s]
+            final_result[item] = temp
 
         return final_result
+    def save_lookups(self):
+        # obj = MeteoExtractor('../Data/METEO/CABOWE', "NL2", {"Apple": 1, "Apple2": 3, "Apple4": 4, "Apple5": 5,
+        #                                                      "Orange1": 1, "Orange2": 2, "Orange3": 4, "Orange4": 5})
+        result = self.load_general_lookup()
+        for item in result.keys():
+            with open(f'./{item}.txt', 'w') as f:
+                temp_str = ''
+                for i in result[item]:
+                    temp_str += f",({result[item].index(i)},{i})"
+                f.write(f"{item} ([(1,{min(result[item])}),({len(result[item])},{max(result[item])})]{temp_str})\n")
 
 
 if __name__ == '__main__':
     obj = MeteoExtractor('../Data/METEO/CABOWE', "NL2", {"Apple": 1, "Apple2": 3, "Apple4": 4, "Apple5": 5,
                                                          "Orange1": 1, "Orange2": 2, "Orange3": 4, "Orange4": 5})
-    obj.writer()
+    result = obj.save_lookups()
 
-    pass
+    # for item in result.keys():
+    #     with open(f'./{item}.txt', 'w') as f:
+    #         temp_str = ''
+    #         for i in result[item]:
+    #             temp_str += f",({result[item].index(i)},{i})"
+    #         f.write(f"{item} ([(1,{min(result[item])}),({len(result[item])},{max(result[item])})]{temp_str})\n")
